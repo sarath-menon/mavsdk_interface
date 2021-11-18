624815f1d75134fd73a3004dd7918a029c019a4c
smenon <smenon@ethz.ch>
2021-11-18 03:06:33 +0100

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+std::shared_ptr<System> get_system(Mavsdk &mavsdk) {
+  std::cout << "Waiting to discover system...\n";
+  auto prom = std::promise<std::shared_ptr<System>>{};
+  auto fut = prom.get_future();
+
+  // We wait for new systems to be discovered, once we find one that has an
+  // autopilot, we decide to use it.
+  mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
+    auto system = mavsdk.systems().back();
+
+    if (system->has_autopilot()) {
+      std::cout << "Discovered autopilot\n";
+
+      // Unsubscribe again as we only want to find one system.
+      mavsdk.subscribe_on_new_system(nullptr);
+      prom.set_value(system);
+    }
+  });
+
+  // We usually receive heartbeats at 1Hz, therefore we should find a
+  // system after around 3 seconds max, surely.
+  if (fut.wait_for(seconds(3)) == std::future_status::timeout) {
+    std::cerr << "No autopilot found.\n";
+    return {};
+  }
+
+  // Get discovered system now.
+  return fut.get();
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() { delete ui; }
---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <chrono>
+#include <cstdint>
+#include <future>
+#include <iostream>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <memory>
+#include <thread>
+
+using namespace mavsdk;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+
+public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+private:
+  Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
